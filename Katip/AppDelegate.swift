9a59ee6874c27443f86d6d1a81848668eee3fcf8
Imdat Solak <imdat@solak.de>
2021-01-24 09:02:58 +0100

    Initial Commit

---
+++ Katip/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Katip
+//
+//  Created by Imdat Solak on 24.01.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
